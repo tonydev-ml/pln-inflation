adcff488584722e1235dcb4a713baac9a52a63d0
Herr Diesel <64426028+HerrDiesel@users.noreply.github.com>
2021-10-09 21:43:05 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,34 +2,25 @@ const test = require("tape")
 
 const inflation = require("./index")
 
-test("should convert $0.25 in 1922 to $3.65 in 2017 if no to argument is provided", t => {
-  const actual = inflation({ year: 1922, amount: 0.25 })
-  t.equal(actual, 3.65)
+test("should convert 10 PLN in 1995 to 8.09 in 2020 if no to argument is provided", t => {
+  const actual = inflation({ year: 1995, amount: 10 })
+  t.equal(actual, 8.09)
   t.end()
 })
 
-test("should convert $0.25 in 1922 to $3.24 in 2010", t => {
-  const actual = inflation({ year: 1922, amount: 0.25 }, { year: 2010 })
-  t.equal(actual, 3.24)
-  t.end()
-})
-
-test("should convert $0.25 in 1922 to $2.38 in March of 1997", t => {
-  const actual = inflation(
-    { year: 1922, amount: 0.25 },
-    { year: 1997, month: 3 }
-  )
-  t.equal(actual, 2.38)
+test("should convert 10 in 1995 to 8.03 in 2010", t => {
+  const actual = inflation({ year: 1995, amount: 10 }, { year: 2010 })
+  t.equal(actual, 8.03)
   t.end()
 })
 
 test("should require a from argument with a year and an amount", t => {
   t.throws(() => inflation(), /from.year must be provided/)
-  t.throws(() => inflation({ year: 1992 }), /from.amount must be provided/)
+  t.throws(() => inflation({ year: 1995 }), /from.amount must be provided/)
   t.end()
 })
 
-test("should only convert prices from 1913 or later", t => {
+test("should only convert prices from 1995 or later", t => {
   t.throws(
     () => inflation({ year: 1900, amount: 1.23 }),
     /from.year must be 1913 or later/
