3e0c0f305c5aee3ec2db2b0d7aeacbdc8253fc91
Herr Diesel <dave.gochnio@gmail.com>
2022-09-20 20:01:51 +0200

    Change the code of montly_inflation() due to recent changes in gus-cpi-data.json

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const data = require("./gus-cpi-data.json");
-const lastFullYearOfData = 2021;
-const lastMonthOfData = "Aug";
+const latestAnnualData = 2021; // year
+const latestMonthlyData = {year: 2022, month: 8}; // year and month
 
 // ANNUAL INFLATION
 function annual_inflation(initialFrom, initialTo) {
   const from = initialFrom || {};
-  const to = initialTo || { year: lastFullYearOfData };
-  if (!from.year) {
+  const to = initialTo || { year: latestAnnualData };
+  
+  if(!from.year){
     throw new Error("from.year must be provided");
-  } else if (!from.amount) {
+  }else if(!from.amount){
     throw new Error("from.amount must be provided");
-  } else if (typeof from.year !== "number") {
+  }else if(typeof from.year !== "number"){
     throw new Error("from.year must be a number, like 2001");
-  } else if (from.year < 1995) {
+  }else if(from.year < 1995){
     throw new Error("from.year must be 1995 or later");
   }
 
   function getCpi(year) {
-    const annual = "Annual";
     const yearData = data.find(d => d.Year === year);
-    return yearData[annual];
+    return yearData["Annual"];
   }
 
   const fromCpi = getCpi(from.year);
@@ -35,37 +35,25 @@ function annual_inflation(initialFrom, initialTo) {
 // MONTHLY INFLATION
 function monthly_inflation(initialFrom, initialTo){
   const from = initialFrom || {};
-  const to = initialTo || { year: lastFullYearOfData, month: lastMonthOfData};
-  if (!from.year) {
+  const to = initialTo || { year: latestMonthlyData.year, month: latestMonthlyData.month};
+  
+  if(!from.year){
     throw new Error("from.year must be provided");
-  } else if (!from.amount) {
+  }else if(!from.month){
+    throw new Error("from.month must be provided");
+  }else if(!from.amount){
     throw new Error("from.amount must be provided");
-  } else if (typeof from.year !== "number") {
+  }else if(typeof from.year !== "number"){
     throw new Error("from.year must be a number, like 2001");
-  } else if (from.year < 1995) {
+  }else if(typeof from.month !== "number"){
+    throw new Error("from.month must be a number from 1 to 12 (months)");
+  }else if(from.year < 1995){
     throw new Error("from.year must be 1995 or later");
   }
-  const months = [
-    { abbr: "Jan", name: "January", nazwa: "Styczeń", order: 1 },
-    { abbr: "Feb", name: "February", nazwa: "Luty", order: 2 },
-    { abbr: "Mar", name: "March", nazwa: "Marzec", order: 3 },
-    { abbr: "Apr", name: "April", nazwa: "Kwiecień", order: 4 },
-    { abbr: "May", name: "May", nazwa: "Maj", order: 5 },
-    { abbr: "Jun", name: "June", nazwa: "Czerwiec", order: 6 },
-    { abbr: "Jul", name: "July", nazwa: "Lipiec", order: 7 },
-    { abbr: "Aug", name: "August", nazwa: "Sierpień", order: 8 },
-    { abbr: "Sep", name: "September", nazwa: "Wrzesień", order: 9 },
-    { abbr: "Oct", name: "October", nazwa: "Październik", order: 10 },
-    { abbr: "Nov", name: "November", nazwa: "Listopad", order: 11 },
-    { abbr: "Dec", name: "December", nazwa: "Grudzień", order: 12 }
-  ];
 
-  function getCpi(input_year, input_month) {
-    const month = input_month
-      ? months.find(m => m.order === input_month).abbr
-      : "Dec";
-    const yearData = data.find(d => d.Year === input_year);
-    return yearData[month];
+  function getCpi(year, month) {
+    const yearData = data.find(d => d.Year === year);
+    return yearData.Monthly[month];
   }
 
   const fromCpi = getCpi(from.year, from.month);
