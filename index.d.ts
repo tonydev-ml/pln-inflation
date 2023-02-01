2a7893de18974c9fff13b79d93bda5663a8ec7be
Herr Diesel <64426028+HerrDiesel@users.noreply.github.com>
2023-02-01 20:08:54 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 export function annual_inflation(initialFrom: any, initialTo: any): number;
 export function monthly_inflation(initialFrom: any, initialTo: any): number;
+export function latest_cpi(): number;
 export { annual_inflation as inflation };
